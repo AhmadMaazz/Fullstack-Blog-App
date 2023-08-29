8ec78b8c13e9f1c30249dab6a2f91a550422869a
Ahmad Maaz <ahmad.maaz273@gmail.com>
2023-08-29 14:12:42 +0500

    navigation setup plus UI for Navbar and footer

---
+++ client/src/App.js
@@ -0,0 +1,75 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Outlet,
+} from "react-router-dom";
+
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Write from "./pages/Write";
+import Home from "./pages/Home";
+import Single from "./pages/Single";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+import "./style.scss";
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/post/:i  d",
+        element: <Single />,
+      },
+      {
+        path: "/write",
+        element: <Write />,
+      },
+    ],
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/",
+    element: <div>Hello World!</div>,
+  },
+  {
+    path: "/",
+    element: <div>Hello World!</div>,
+  },
+]);
+
+function App() {
+  return (
+    <div className="app">
+      <div className="container">
+        <RouterProvider router={router} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import logo from "../images/logo.png";
+
+export const Footer = () => {
+  return (
+    <footer>
+      <img src={logo} alt="" />
+      <span>
+        Made with ❤ and <b>React.js</b>
+      </span>
+    </footer>
+  );
+};
+
+export default Footer;
